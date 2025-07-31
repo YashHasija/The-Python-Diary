399807e554e4bb7f0e596437159b11fb08037265
YashHasija <hasijayashwork@gmail.com>
2025-07-31 15:34:38 +0530

    leftover

---
+++ datatypes/chapter_string_5.py
@@ -0,0 +1,9 @@
+chai_type = "Ginger chai"
+customer_name = "priya"
+
+print(f'order for {customer_name} : {chai_type} please !')
+
+chai_description = "aromatic and bold"
+print(f'first word: {chai_description[:8]}') #indexing and slicing
+print(f'last word: {chai_description[12:]}')
+print(f'last word: {chai_description[::-1]}')
