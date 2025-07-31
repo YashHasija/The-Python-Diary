ef170f7335ff9e8b1b7b466101720474caca23c2
YashHasija <hasijayashwork@gmail.com>
2025-07-31 14:44:22 +0530

    real floating decimal

---
+++ datatypes/chapter_4.py
@@ -0,0 +1,9 @@
+import sys
+
+ideal_temprature = 95.5
+current_temp = 95.49
+
+print(f'ideal temp {ideal_temprature}')
+print(f'current temp {current_temp}')
+print(f'difference temp {ideal_temprature - current_temp}')
+print(sys.float_info)
